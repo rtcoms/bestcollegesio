b1a9ca12c050b8faa550ca6038f39b41539c774b
Rohit Trivedi <86.rohit@gmail.com>
2014-10-02 00:03:41 +0530

    using postgres as database

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20140930050913) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "api_responses", force: true do |t|
-<<<<<<< HEAD
-    t.string   "url",        null: false
-    t.text     "response",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-=======
     t.string   "url",           null: false
     t.text     "response"
     t.text     "error_message"
     t.boolean  "success"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
->>>>>>> api response table created
   end
 
   create_table "users", force: true do |t|
@@ -44,7 +40,7 @@ ActiveRecord::Schema.define(version: 20140930050913) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
